35d1d7da035c332e8e9758b60e9f696bfb9950b3
JalajaUtekar <jalaja.utekar@gmail.com>
2025-08-06 02:25:58 +0530

    Assignment 2: Game Development
    Problem Statement: 
    Write a game development program that implements the Bubble Sort algorithm. The program will simulate a simple game where the player can input a set of numbers, and the numbers will be sorted using Bubble Sort to simulate a "level-up" scenario where the player's scores are sorted in ascending order.

--- Game Development.cpp
+++ Game Development.cpp
@@ -1,62 +1,55 @@
 //Problem Statement: Write a game development program that implements the Bubble Sort algorithm. The program will simulate a simple game where the player can input a set of numbers, and the numbers will be sorted using Bubble Sort to simulate a "level-up" scenario where the player's scores are sorted in ascending order.
 #include <iostream> 
-using namespace std;
-int main(){ 
-	int r,i,j,t = 0, sum1 = 0, sum2 = 0;
-	cout<<"Enter number of rounds: ";
-	cin>>r;
-	int player1[r], player2[r];
-	cout<<"Enter Player 1's scores: "<<endl;
-	for (i=0;i<r;i++) {
-		cout<<"Round "<<i+1<<": ";
-		cin>>player1[i];
-	}
-	cout<<"Enter Player 2's scores: "<<endl;
-	for (i=0;i<r;i++) {
-		cout<<"Round "<<i+1<<": ";
-		cin>>player2[i];
-	}
-	for (i=0;i<r;i++) {
-		for (j=0;j<r-i-1;j++) {
-			if (player1[j] < player1[j+1]) {
-				t = player1[j];
-				player1[j] = player1[j+1];
-				player1[j+1] = t;
-			}
-		}
-	}
-	for (i=0;i<r;i++) {
-		for (j=0;j<r-i-1;j++) {
-			if (player2[j] < player2[j+1]) {
-				t = player2[j];
-				player2[j] = player2[j+1];
-				player2[j+1] = t;
-			}
-		}
-	}
-	for (i=0;i<r;i++) {
-		sum1 += player1[i];
-		sum2 += player2[i];
-	}
-	cout<<"-------------------------------------------------"<<endl;
-	cout<<"Player 1's sorted scores"<<endl;
-	for (i=0;i<r;i++) {
-		cout<<player1[i]<<"\t";
-	}
-	cout<<"\n-------------------------------------------------"<<endl;
-	cout<<"Player 2's sorted scores"<<endl;
-	for (i=0;i<r;i++) {
-		cout<<player2[i]<<"\t";
-	}
-	cout<<"\n-------------------------------------------------"<<endl;
-	if (sum1 == sum2) {
-		cout<<"\n\nTIE!";
-	}
-	else if (sum2>sum1) {
-		cout<<"\n\nPLAYER 2 WINS!";
-	}
-	else {
-		cout<<"\n\nPLAYER 1 WINS!";
-	}
-	return 0;
-}
+using namespace std; 
+int main () { 
+    	int player1[5]; 
+  	int player2[5]; 
+   	int a; 
+    	cout << "Enter the scores of 5 rounds for Player 1: " << endl; 
+    	for (int i=0; i < 5; i++) { 
+        	cin >> player1[i]; 
+    	}  
+    	cout << "\n\nEnter scores of 5 rounds for Player 2: " << endl; 
+    	for (int i=0; i < 5; i++) { 
+        	cin >> player2[i]; 
+    	} 
+    	for (int pass=0; pass < 5; pass++) { 
+        	for (int i=0; i < (5-pass-1); i++) { 
+            		if (player1[i] < player1[i+1]) { 
+                			a = player1[i]; 
+                			player1[i] = player1[i+1]; 
+                			player1[i+1] = a; 
+            		} 
+        	} 
+   	 } 
+    	for (int pass=0; pass < 5; pass++) { 
+       		for (int i=0; i < (5-pass-1); i++) { 
+            		if (player2[i] < player2[i+1]) { 
+                			a = player2[i]; 
+                			player2[i] = player2[i+1]; 
+                			player2[i+1] = a; 
+            		} 
+        	} 
+    	} 
+    	cout << "Swapping scores and leveling up!!!" << endl; 
+    	cout << "Scores have been swapped!" << endl; 
+    	cout << "The sorted scores are: " << endl; 
+    	cout << "Scores of Player 1: "; 
+    	for (int i=0; i < 5; i++) { 
+       	 	cout << player1[i] << ", "; 
+    	} 
+    	cout << endl; 
+    	cout << "Scores of Player 2: "; 
+    	for (int i=0; i < 5; i++) { 
+        	cout << player2[i] << ", "; 
+    	} 
+    	cout << endl; 
+    	if (player1[0] > player2[0]) { 
+       	 	cout << "Player 1 is the winner!" << endl; 
+    	} else if (player2[0] > player1[0]) { 
+        	cout << "Player 2 is the winner!" << endl; 
+    	}  else { 
+       		 cout << "Its a tie!" << endl; 
+    	} 
+    	return 0; 
+} 
